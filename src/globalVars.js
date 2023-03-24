d66566989489ae9c30685310e3698b050781d532
Ghazal Alabtah <73082092+alabgh01@users.noreply.github.com>
2023-03-24 14:19:09 -0500

    Update globalVars.js

--- src/globalVars.js
+++ src/globalVars.js
@@ -1,5 +1,5 @@
 /*jshint esversion: 8*/
 export default {
-  //   PORT: "https://sp-backend-b70z.onrender.com/api/v1",
-  PORT: "http://localhost:5000/api/v1",
+     PORT: "https://sp-backend-b70z.onrender.com/api/v1",
+  // PORT: "http://localhost:5000/api/v1",
 };
