3e37c1b45ab3751b60dd04f16497aab0240d2062
alabgh01 <alabgh01@luther.edu>
2023-04-30 23:55:10 -0500

    deleting playwright stuff

--- playwright/index.jsx
+++
@@ -1,9 +0,0 @@
-// Import styles, initialize component theme here.
-// import '../src/common.css';
-import "../src/components/post/Post.css";
-import "../src/components/post/Reply.css";
-import React, { useState, useEffect } from "react";
-import { MdInsertComment } from "react-icons/md";
-import { MdAddComment } from "react-icons/md";
-import { GoPrimitiveDot } from "react-icons/go";
-import { Link } from "react-router-dom";
